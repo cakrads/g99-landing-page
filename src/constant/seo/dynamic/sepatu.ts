cea0825fb76910661b1458dcbe4526ffe9d4d983
Cakra DS <cakra.ds@gmail.com>
2024-11-24 20:33:23 +0700

    enhance meta

--- src/constant/seo/dynamic/sepatu.ts
+++ src/constant/seo/dynamic/sepatu.ts
@@ -8,7 +8,7 @@ export const SLUG = "laundry-sepatu";
 
 export const webSeo: IWebSeo = {
   title: WEB_TITLE,
-  description: "Rawat sepatu kesayangan Anda di Laundry G99: Laundry Sepatu Medan Marelan. Aman untuk berbagai bahan dan mempertahankan keindahan dan usia sepatu. Pesan sekarang!",
+  description: "Laundry sepatu kesayangan Anda agar terlihat seperti baru lagi di Laundry G99: Laundry Sepatu Medan Marelan. Pasti bersih dan Aman. Pesan sekarang!",
   primaryKeyword: [
     "laundry sepatu",
     "cuci sepatu",
