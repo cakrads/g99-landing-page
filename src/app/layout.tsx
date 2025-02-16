49422af6b69c7bcf8928bc0810518bb0246a7907
Cakra DS <cakra.ds@gmail.com>
2025-02-16 23:48:55 +0700

    change lcp to h1, enhance font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,8 +54,8 @@ export default function RootLayout({
       <body
         className={cn(
           "antialiased",
-          fontHeading.variable,
-          fontBody.variable
+          fontHeading.className,
+          fontBody.className,
         )}
       >
         <NextTopLoader
