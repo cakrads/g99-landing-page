188227bc6cdd578aeabfb85ef399f014ab386f7f
Cakra DS <cakra.ds@gmail.com>
2025-02-16 22:15:38 +0700

    enhance LCP, enhance tailwind in prod

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
+// https://v3.tailwindcss.com/docs/optimizing-for-production
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   },
 };
